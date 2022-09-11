89e1344d5486763d58644758d977103ce2146ad7
Ram-143-sai <saiyerdesi7@gmail.com>
2022-09-11 11:36:03 +0530

    added routing and static student part

--- t4-supply-chain-react/src/App.js
+++ t4-supply-chain-react/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import CollegeInfo from './Components/CollegeInfo';
+import StudentInfo from './Components/StudentInfo';
+import {
+  BrowserRouter as Router, 
+  Routes, 
+  Route
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <CollegeInfo />
-    </div>
-  );
+
+const App = () => {
+  return (<>
+  <Router>
+  <Routes>
+
+    <Route exact path="/" element={<StudentInfo />} />
+    <Route exact path="/CollegeInfo" element={<CollegeInfo />} />
+    
+  </Routes>
+  </Router> 
+  </>)
 }
 
-export default App;
+export default App
---
+++ t4-supply-chain-react/src/Components/StudentInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+class HelloMessage extends React.Component {
+    render() {
+      return <div>Hello </div>;
+    }
+  }
+  
+export default HelloMessage
